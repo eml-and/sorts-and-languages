6e6676385123bec3de8bbf7e4cf373eadc754440
emila <emil.andreas@stud.tu-darmstadt.de>
2022-06-19 14:57:17 +0200

    added quick_sort.py

--- bubble_sorts/bubble_sort.py
+++ bubble_sorts/bubble_sort.py
@@ -1,5 +1,5 @@
 # O(n^2)
-the_list = [42, 69, 12, 99, 5]
+the_list = [42, 360, 69, 2012, 12, 99, 5, 2049, 420]
 print(the_list)
 for i in range(len(the_list)):
     for j in range(i+1, len(the_list)):
---
+++ quick_sorts/quick_sort.py
@@ -0,0 +1,35 @@
+# O(n*log(n)) on average
+
+def divide(array: list, start, end):
+    pivot = array[start]
+    smaller = start+1
+    larger = end
+    while True:
+        while smaller <= larger and array[larger] >= pivot:
+            larger -= 1
+        while smaller <= larger and array[larger] <= pivot:
+            smaller += 1
+        if smaller <= larger:
+            array[smaller], array[larger] = array[larger], array[smaller]
+        else:
+            break
+    array[start], array[larger] = array[larger], array[start]
+    print(array)
+    return larger
+
+
+def quick_sort(array: list, start, end):
+    if start >= end:
+        return
+    pivot = divide(array, start, end)
+    quick_sort(array, start, pivot-1)
+    quick_sort(array, pivot+1, end)
+
+
+if __name__ == "__main__":
+    the_list = [42, 360, 69, 2012, 12, 99, 5, 2049, 420]
+    print(the_list)
+    quick_sort(the_list, 0, len(the_list)-1)
+    print(the_list)
+
+    
