44098cf7135d54f89ef73605e084fcd1a5867997
Emil <emil.andreas@stud.tu-darmstadt.de>
2022-07-12 14:58:34 +0200

    added selection sort in c++

--- selection_sorts/selection_sort.py
+++ selection_sorts/selection_sort.py
@@ -2,12 +2,14 @@
 the_list = [42, 360, 69, 2012, 12, 99, 5, 2049, 420]
 
 print(the_list)
+k = 0
 for j in range(len(the_list)):
+    k = j
     for i in range(j, len(the_list)):
-        min_value = the_list[j]
-        if the_list[i] < min_value:
-            the_list[i], the_list[j]  = the_list[j], the_list[i]
-        print(the_list)
+        if the_list[i] < the_list[k]:
+            k = i
+    the_list[k], the_list[j] = the_list[j], the_list[k]
+    print(the_list)
     print("-----------")
 
 
