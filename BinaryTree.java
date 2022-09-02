abd10d9fb183d63a708ecfa79757ded940139869
Emil And <87652270+eml-and@users.noreply.github.com>
2022-09-02 15:34:49 +0200

    Create BinaryTree.java

---
+++ BinaryTree.java
@@ -0,0 +1,14 @@
+public class BinaryTreeNode<T> {
+  public T key;
+  public BinaryTreeNode<T> left;
+  public BinaryTreeNode<T> right;
+}
+
+// sort the binary tree to degreade it until linearity
+// node.right == null for all nodes
+public void makeLinear(BinaryTreeNode<T> root) {
+ if(root==null) {
+   return;
+ }
+  
+}
